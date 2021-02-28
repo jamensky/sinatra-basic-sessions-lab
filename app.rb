e44498bf73672d55e128d0c78c77de955ce505ae
Jamal <jamensky1991@gmail.com>
2021-02-28 14:34:15 +0000

    session

--- app.rb
+++ app.rb
@@ -1,4 +1,26 @@
 require_relative 'config/environment'
+require 'securerandom'
 
 class App < Sinatra::Base
+ @@secret = SecureRandom.hex(64)
+
+    get '/sess' do
+      @session = session 
+    end 
+
+    configure do 
+        enable :sessions
+        set :session_secret, @@secret
+    end 
+
+    get '/' do 
+      erb :index 
+    end 
+
+    post '/checkout' do 
+      session[:item] = params[:item]
+      p "#{session[:item]}"  
+    end
+
+
 end
